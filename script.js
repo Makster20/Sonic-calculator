fa7dc0837ddf17bf61317a9554d27fdff6994516
Makster20 <mikhaelsaidshah123@gmail.com>
2024-10-23 15:13:52 +0400

    added 2nd number input

--- script.js
+++ script.js
@@ -1,24 +1,60 @@
+
+
+
 const numfield = document.querySelector('.numfield');
+const expressions = document.querySelector('.expressions')
 const buttons = document.querySelectorAll('.button');
 let num1 = '';
 let num2 = '';
 let expression = '';
+let expressionAns = 0;
+let isNum2 = false;
 
 buttons.forEach(button => {
     button.addEventListener('click', function() {
         if(!isNaN(button.innerHTML)){
-            num1 += button.innerHTML;
-            console.log(num1);
-            if(numfield.innerHTML === '0'){
-                numfield.innerHTML = button.innerHTML;
+            if(isNum2 === false){
+                num1 += button.innerHTML;
+
+                if(numfield.innerHTML === '0'){
+                    numfield.innerHTML = button.innerHTML;
+                }
+                else{
+                    numfield.innerHTML += button.innerHTML;
+                }
             }
             else{
-                numfield.innerHTML += button.innerHTML;
+                num2 += button.innerHTML;
+                if(numfield.innerHTML === num1){
+                    numfield.innerHTML = button.innerHTML;   
+                }
+                else{
+                    numfield.innerHTML += button.innerHTML;
+                }
             }
         }
         else{
+            if (button.innerHTML === 'AC'){
+                
+            }
+            else if(button.innerHTML === 'DEL'){
 
-            
+            }
+            else if(button.innerHTML === '='){
+
+            }
+            else{
+                if(numfield.innerHTML === num1){
+                    expression += `${num1} ${button.innerHTML} `;
+                    expressions.innerHTML = expression;
+                    isNum2 = true;
+                }
+                else if(numfield.innerHTML === num2){
+                    expression += num2
+                    expressionAns = evaluate(expression)
+                    console.log(expressionAns)
+                }
+            }
 
         }
         
